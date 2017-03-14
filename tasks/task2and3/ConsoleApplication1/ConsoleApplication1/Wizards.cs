6ca28cdca9a170e9c1519fd52472291f864fa4db
Pawel Kowalski <ic16b027@TECHNIKUM-WIEN.AT>
2017-03-14 17:06:47 +0100

    adding task2and3

---
+++ tasks/task2and3/ConsoleApplication1/ConsoleApplication1/Wizards.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2and3
+{ interface Wizards
+    {
+        string name { get; }
+        string spell { get; }
+        int powerlvl { get; }
+        void power (int powerlvl);
+    }
+}
