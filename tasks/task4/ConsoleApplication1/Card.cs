1704f8cae6464daf4352f0e02f631d3eb487ce28
Pawel Kowalski <kowalski.pawel21@gmail.com>
2017-03-28 01:49:39 +0200

    new version

---
+++ tasks/task4/ConsoleApplication1/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{ interface Card
+    {
+        string name { get; }
+        int mana { get; }
+        int powerlvl { get; }
+
+        void text ();
+    }
+}
---
+++ tasks/task4/ConsoleApplication1/Creature.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Creature:Card
+    {
+        public int defenselvl;
+        public string typus;
+        public Creature(string c_name, int c_mana, int c_powerlvl, int c_defenselvl, string c_typus)
+        {
+            name = c_name;
+            mana = c_mana;
+            powerlvl = c_powerlvl;
+            defenselvl = c_defenselvl;
+            typus = c_typus;
+        }
+        public string name { get; }
+        public int mana { get; }
+        public int powerlvl { get; }
+
+        public void text()
+        {
+            if (powerlvl < defenselvl)
+            {
+                Console.WriteLine("Tank Card\n");
+            }
+            else if (powerlvl > defenselvl)
+            {
+                Console.WriteLine("Slayer Card\n");
+            }
+            else
+                Console.WriteLine("Guardian Card\n");
+        }
+    }
+}
